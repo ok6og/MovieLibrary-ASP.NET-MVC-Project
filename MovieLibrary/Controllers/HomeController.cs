1104e4813fa2ff1de12273edb124bccf89312b4f
ok6og <ok6og17@gmail.com>
2022-03-18 23:23:04 +0200

    Add project files.

---
+++ MovieLibrary/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieLibrary.Core.Constants;
+using MovieLibrary.Models;
+using System.Diagnostics;
+
+namespace MovieLibrary.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData[MessageConstant.ErrorMessage] = "Нещо се счупи брат!";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
