88781040e87bde6df8cb9b95da2c942326b2d748
ok6og <ok6og17@gmail.com>
2022-04-24 02:27:18 +0300

    initializing api

---
+++ MovieLibrary/Controllers/Api/MovieApiController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieLibrary.Infrastructure.Data;
+
+namespace MovieLibrary.Controllers.Api
+{
+    [ApiController]
+    [Route("api/movies")]
+    public class MovieApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext data;
+
+        public MovieApiController(ApplicationDbContext data)
+        {
+            this.data = data;
+        }
+
+
+
+    }
+}
