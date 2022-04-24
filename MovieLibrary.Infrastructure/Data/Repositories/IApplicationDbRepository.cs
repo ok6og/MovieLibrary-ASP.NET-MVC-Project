497ef4b311cb3671923413a7e3993a8cd0e3212f
ok6og <ok6og17@gmail.com>
2022-04-24 19:26:43 +0300

    Interface for app db repo

---
+++ MovieLibrary.Infrastructure/Data/Repositories/IApplicationDbRepository.cs
@@ -0,0 +1,13 @@
+﻿using MovieLibrary.Infrastructure.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLibrary.Infrastructure.Data.Repositories
+{
+    public interface IApplicatioDbRepository : IRepository
+    {
+    }
+}
