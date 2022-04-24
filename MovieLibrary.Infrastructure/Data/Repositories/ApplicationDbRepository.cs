2201da5c861a4e5b19daba6926c3031e04ec8449
ok6og <ok6og17@gmail.com>
2022-04-24 19:25:33 +0300

    AppDbRepo

---
+++ MovieLibrary.Infrastructure/Data/Repositories/ApplicationDbRepository.cs
@@ -0,0 +1,17 @@
+﻿using MovieLibrary.Infrastructure.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLibrary.Infrastructure.Data.Repositories
+{
+    public class ApplicatioDbRepository : Repository, IApplicatioDbRepository
+    {
+        public ApplicatioDbRepository(ApplicationDbContext context)
+        {
+            this.Context = context;
+        }
+    }
+}
